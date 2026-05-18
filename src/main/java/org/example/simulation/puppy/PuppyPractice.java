3971173f2e04d6835e09b05c8a0647a19cf56558
leeheejae95 <leeheejae55@naver.com>
2026-05-18 18:33:52 +0900

    feat : 잃어버린 강아지 시뮬레이션 풀이 - 잃어버린 강아지 방향 시뮬레이션 (BFS 아닌 방향전환 방식)

--- src/main/java/org/example/simulation/puppy/PuppyPractice.java
+++ src/main/java/org/example/simulation/puppy/PuppyPractice.java
@@ -3,48 +3,57 @@ package org.example.simulation.puppy;
 public class PuppyPractice {
     public int solution(int[][] board){
         int answer = 0;
+        int n = board.length;
         int[] dx = {-1,0,1,0};
         int[] dy = {0,1,0,-1};
-        int n = board.length;
-        int x1=0,y1=0,x2=0,y2=0,cnt=0;
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                if(board[i][j]==2)  {
+        int x1=0,y1=0,x2=0,y2=0,cnt=0,d1=0,d2=0;
+
+        for(int i=0; i< n; i++) {
+            for(int j=0; j< n; j++) {
+                if(board[i][j]==2) { // 현수 좌표
                     x1 = i;
                     y1 = j;
-                } else if(board[i][j]==3)  {
+                } else if(board[i][j]==3) { // 강아지 좌표
                     x2 = i;
                     y2 = j;
                 }
             }
         }
-        int d1=0,d2=0;
-        while(cnt < 10000){
-            cnt++;
+
+        while (cnt < 10000) {
+            // 현수 좌표
             int nx1 = x1 + dx[d1];
             int ny1 = y1 + dy[d1];
+            // 강아지 좌표
             int nx2 = x2 + dx[d2];
             int ny2 = y2 + dy[d2];
             boolean flag1=true, flag2=true;
-            if(nx1<0 || nx1>=n ||  ny1<0 || ny1>=n || board[nx1][ny1]==1) {
+
+            if(nx1 < 0 || nx1 >= n || ny1 < 0 || ny1 >= n || board[nx1][ny1] == 1) {
                 d1 = (d1 + 1) % 4;
                 flag1 = false;
             }
-            if(nx2<0 || nx2>=n ||  ny2<0 || ny2>=n || board[nx2][ny2]==1) {
+            if(nx2 < 0 || nx2 >= n || ny2 < 0 || ny2 >= n || board[nx2][ny2] == 1) {
                 d2 = (d2 + 1) % 4;
                 flag2 = false;
             }
+            // board범위 안일 때 죄표 넣기
             if(flag1) {
                 x1 = nx1;
                 y1 = ny1;
             }
+
             if(flag2) {
                 x2 = nx2;
                 y2 = ny2;
             }
-            if(cnt > 10000) return 0;
-            if(x1==x2 && y1==y2) break;
+
+            cnt++;
+
+            if(x1==x2 && y1==y2) return cnt;
         }
+
+        if(cnt > 10000) return 0;
 
         return cnt;
     }
